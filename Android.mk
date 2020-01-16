7ea53f20193114e0041c2a312e09446b761a54c2
Kai Jones <zaksenoj@icloud.com>
2020-01-16 06:58:54 -0800

    karnak:remove uneeded stuff in Android.mk since it is going to be moved
    Its going to be moved to Android.bp later.
    
    Change-Id: Ie6bf21b462c8abfaf2f173fa4095a8d4d57a41d9

--- Android.mk
+++ Android.mk
@@ -18,35 +18,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),karnak)
 
-$(info [Karnak] including vendor blobs....)
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libcam.utils.sensorlistener
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_OWNER = mtk
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_PROPRIETARY_MODULE = true
-LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libcam.utils.sensorlistener.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libion_mtk
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libion_mtk.so
-LOCAL_PROPRIETARY_MODULE = true
-LOCAL_MULTILIB := both
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libged
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libged.so
-LOCAL_PROPRIETARY_MODULE = true
-LOCAL_MULTILIB := both
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
 endif
